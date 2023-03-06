64a57e3e7c23f8e2d4d06ace744b34139c89138e
mza-codes <muzofficial90@gmail.com>
2023-03-06 14:34:14 +0530

    SSR form

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const fields = [
+  {
+    name: "name",
+    placeholder: "Your Name",
+    type: "text",
+    maxlength: "20",
+    minlength: "3"
+  },
+  {
+    name: "email",
+    placeholder: "Your Email",
+    maxlength: "50",
+    type: "email"
+  },
+  {
+    name: "bio",
+    placeholder: "Your Intro",
+    type: "text",
+    maxlength: "60",
+    minlength: "10"
+  },
+  {
+    name: "links",
+    placeholder: "Your Links",
+    type: "text",
+    minlength: "10"
+  }
+];
+
+router.get('/', function (req, res, next) {
+
+  res.render('index', { title: 'Generate My Site', fields });
 });
 
 module.exports = router;
