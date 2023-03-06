1d508c4207f75f2501bc30c0dbe7bf69ff44a836
mza-codes <muzofficial90@gmail.com>
2023-03-06 16:34:34 +0530

    Added module.js,Added interactivity

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-const fields = [
-  {
-    name: "name",
-    placeholder: "Your Name",
-    type: "text",
-    maxlength: "20",
-    minlength: "3"
-  },
-  {
-    name: "email",
-    placeholder: "Your Email",
-    maxlength: "50",
-    type: "email"
-  },
-  {
-    name: "bio",
-    placeholder: "Your Intro",
-    type: "text",
-    maxlength: "60",
-    minlength: "10"
-  },
-  {
-    name: "links",
-    placeholder: "Your Links",
-    type: "text",
-    minlength: "10"
-  }
-];
+const fields = [{
+  name: "name",
+  placeholder: "Your Name",
+  type: "text",
+  maxlength: "20",
+  minlength: "3"
+},
+{
+  name: "email",
+  placeholder: "Your Email",
+  maxlength: "50",
+  type: "email"
+},
+{
+  name: "bio",
+  placeholder: "Your Intro",
+  type: "text",
+  maxlength: "60",
+  minlength: "10"
+}, {
+  name: "image",
+  placeholder: "Picture URL",
+  type: "text",
+  minlength: "10"
+},];
+
+const linkFields = [{
+  name: "links",
+  placeholder: "Your Links",
+  type: "text",
+  minlength: "10"
+}];
 
 router.get('/', function (req, res, next) {
 
